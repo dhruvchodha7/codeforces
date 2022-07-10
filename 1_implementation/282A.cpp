069b5280a1a2da4e138f36179d5c84100affaaae
dhruvchodha7 <dhruvchodha@live.com>
2022-07-10 20:37:00 +0530

    initial commit

---
+++ 1_implementation/282A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ 
+int main(){
+    int n, x=0;
+    cin>>n;
+    
+    string s;
+    
+    while(n--){
+        cin>>s;
+        if(s[i]=='+') ++x;
+        else --x;
+    }
+    cout<<x<<endl;
+    return 0;
+}
