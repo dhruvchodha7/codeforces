a7927d10817a91d9b5897fb29ab98ffd7e6bde4f
dhruvchodha7 <dhruvchodha@live.com>
2022-07-12 22:39:21 +0530

    completed 1030A

---
+++ 1030A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n, i;
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> i;
+        if (i == 1)
+        {
+            cout << "HARD" << endl;
+            return 0;
+        }
+    }
+    cout << "EASY" << endl;
+
+    return 0;
+}
