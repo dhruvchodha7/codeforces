b710e2b1f6856e06cbf56f1dd1ab7bb2b02d3d40
dhruvchodha7 <dhruvchodha@live.com>
2022-07-13 17:04:33 +0530

    completed 546A soilder and bananas

---
+++ 546A_SoilderAndBananas.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, w;
+    long long sum = 0;
+    cin >> k >> n >> w;
+    for (int i = 1; i <= w; i++)
+    {
+        sum = sum + (i * k);
+    }
+    if (sum <= n)
+        cout << 0;
+    else
+        cout << sum - n;
+
+    return 0;
+}
