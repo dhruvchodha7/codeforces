3657ce55e3f71fc800c2c22d54e82120b9a9d753
dhruvchodha7 <dhruvchodha@live.com>
2022-07-15 14:04:46 +0530

    completed 492A Vanya and Cubes

---
+++ 492A_VanyaAndCubes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int n){
+    return n*(n+1)/2;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int level=0;
+    while (1)
+    {
+       n-=sum(level+1);
+       if(n>=0) level++;
+       else break;
+    }
+    cout<<level;
+    return 0;
+}
