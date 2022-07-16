45969e34bd2f305ea7761c3c5e534d0988e5be4b
dhruvchodha7 <dhruvchodha@live.com>
2022-07-16 15:29:24 +0530

    completed 1433A Boring Apartments

--- 1433A_BoringApartments.cpp
+++ 1433A_BoringApartments.cpp
@@ -1,37 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    int n;
+    while (t--)
     {
-        int a;
-        cin>>a;
-        int A=a;
-        int digit=a%10;
-        int cnt=0;
-        while(a>0)
+        cin>>n;
+        int digit = n%10;
+        int res;
+        res = (digit-1)*10;
+        int num = digit;
+        int i=1;
+        while (num<=n)
         {
-            a/=10;
-            cnt++;
+            res+=(i++);
+            num=num*10+digit;
         }
-
-        if(cnt==1)
-        {
-            cout<<((digit*10)-10)+1<<endl;
-        }
-        else if(cnt==2)
-        {
-            cout<<((digit*10)-10)+3<<endl;
-        }
-        else if(cnt==3)
-        {
-            cout<<((digit*10)-10)+6<<endl;
-        }
-        else if(cnt==4)
-        {
-            cout<<((digit*10)-10)+10<<endl;
-        }
+        cout<<res<<endl;
+        
     }
+    return 0;
 }
