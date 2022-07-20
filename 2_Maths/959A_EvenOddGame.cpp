5c87903da1e05cdf9ae3b268f011d71073332c7a
dhruvchodha7 <dhruvchodha@live.com>
2022-07-20 20:18:06 +0530

    completed 959A Even odd game

---
+++ 2_Maths/959A_EvenOddGame.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    //if n is even print "Mahmoud" else print "Ehab"
+    if(n%2 == 0)
+        cout<<"Mahmoud"<<endl;
+    else
+        cout<<"Ehab"<<endl;
+    return 0;
+}
