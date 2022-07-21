7e623beec0e9a07526a2672542d9b4bacecfa3d1
dhruvchodha7 <dhruvchodha@live.com>
2022-07-21 12:21:24 +0530

    completed 1296A Array with odd sum

---
+++ 2_Maths/1358A_ParkLighting.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    double n,m;
+    cin>>t;
+    while(t--){
+        cin>>n>>m;
+        cout<<ceil((n*m)/2)<<endl;
+    }
+
+    return 0;
+}
