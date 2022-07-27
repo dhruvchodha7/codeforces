e0f2391ad4b072ea5a18cb5356073326afc290cd
dhruvchodha7 <dhruvchodha@live.com>
2022-07-27 14:17:31 +0530

    completed 1335A Candies and Two sisters

---
+++ 2_Maths/1335A_CandiesAndTwoSisters.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        long long n;
+        cin>>n;
+        n = ceil((n/2)-1);
+        cout<<n<<endl;
+    }
+    
+    return 0;
+}
