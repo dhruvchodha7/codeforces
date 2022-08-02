f1544b0f54682aa5078fb7b8b163d0f9a2d22659
dhruvchodha7 <dhruvchodha@live.com>
2022-08-03 00:45:29 +0530

    completed heating

---
+++ 2_Maths/1266B_DiceTower.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll t, num;
+    cin >> t;
+    while (t--)
+    {
+        cin >> num;
+        ll x = num%14;
+        if ((num>14) && (x>=1 ))
+        {
+            /* code */
+        }
+        
+    }
+    
+
+    return 0;
+}
